3449644e663a42f8e46557a071dcf4ae609d7844
joseph wainaina <josephynaina@gmail.com>
2022-11-15 13:46:19 +0000

    files created

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int _putchar(char c);
+
+#endif /*main.h*/
